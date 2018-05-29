d934cb9e1ed98e7898e129d0debc8f40707c7067
Palash Mondal <palashm@live.in>
2018-05-29 20:46:52 +0530

    Read Data from Firebase

--- src/App.vue
+++ src/App.vue
@@ -1,60 +1,121 @@
 <template>
-  <div id="app">
-    <img src="./assets/logo.png">
-    <h1>{{ msg }}</h1>
-    <h2>Essential Links</h2>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank">Twitter</a></li>
-    </ul>
-    <h2>Ecosystem</h2>
-    <ul>
-      <li><a href="http://router.vuejs.org/" target="_blank">vue-router</a></li>
-      <li><a href="http://vuex.vuejs.org/" target="_blank">vuex</a></li>
-      <li><a href="http://vue-loader.vuejs.org/" target="_blank">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank">awesome-vue</a></li>
-    </ul>
+  <div id="app" class="container">
+    <h1>Sample Chat Room</h1>
+    <!-- Messages -->
+    <div v-for="(message, index) in messages" class="card" :key="index">
+      <div class="card-body">
+        <!-- nickname -->
+        <h6 class="card-subtitle mb-2 text-muted">{{ message.nickname }}</h6>
+        <!-- content -->
+        <p v-if="message !== editingMessage" class="card-text">{{ message.text }}</p>
+        <textarea v-else v-model="messageText" class="form-control"></textarea>
+        <!-- actions -->
+        <div v-if="message !== editingMessage">
+          <a @click.prevent="deleteMessage(message)" href="#" class="card-link">Delete</a>
+          <a @click.prevent="editMessage(message)" href="#" class="card-link">Edit</a>
+        </div>
+        <div v-else>
+          <a @click.prevent="cancelEditing" href="#" class="card-link">Cancel</a>
+          <a @click.prevent="updateMessage" href="#" class="card-link">Update</a>
+        </div>
+      </div>
+    </div>
+
+    <hr>
+    <!-- New Message -->
+    <form v-if="!editingMessage" @submit.prevent="storeMessage">
+      <div class="form-group">
+        <label>Message:</label>
+        <textarea v-model="messageText" class="form-control"></textarea>
+      </div>
+      <div class="form-group">
+        <label>Nickname:</label>
+        <input v-model="nickname" class="form-control"/>
+      </div>
+      <button class="btn btn-primary">Send</button>
+    </form>
   </div>
 </template>
 
 <script>
+
+const config = require('../config');
+firebase.initializeApp(config);
+const database = firebase.database();
+const messagesRef = database.ref('messages');
+
 export default {
   name: 'app',
   data () {
     return {
-      msg: 'Welcome to Your Vue.js App'
+      messages: [],
+      messageText: '',
+      nickname: 'palash',
+      editingMessage: null
     }
+  },
+  methods: {
+    storeMessage () {
+
+    },
+    deleteMessage (message) {
+
+    },
+    editMessage (message) {
+
+    },
+    cancelEditing () {
+
+    },
+    updateMessage () {
+
+    }
+  },
+  created () {
+
+    // Read Data from Firebase
+    // value = snapshot.val() | key = snapshot.key
+    messagesRef.on('child_added', snapshot => {
+      this.messages.push({...snapshot.val(), id: snapshot.key})
+      if (snapshot.val().nickname !== this.nickname) {
+        nativeToast({
+            message: `New message by ${snapshot.val().nickname}`,
+            type: 'success'
+        })
+      }
+    })
   }
 }
 </script>
 
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Helvetica, Arial, sans-serif, 'Apple Color Emoji',
+    'Segoe UI Emoji', 'Segoe UI Symbol';
+  font-size: 14px;
+  line-height: 1.5;
+  color: #24292e;
+  background-color: #fff;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  margin-top: 20px;
 }
-
-h1, h2 {
-  font-weight: normal;
+.card {
+  box-shadow: 3px 2px 10px 0px rgba(0, 0, 0, 0.35);
+  margin-bottom: 10px;
+  transition: box-shadow 0.3s ease-in-out;
 }
-
-ul {
-  list-style-type: none;
-  padding: 0;
+.card:hover {
+  box-shadow: 3px 2px 10px 0px rgba(0, 0, 0, 0.63);
 }
-
-li {
-  display: inline-block;
-  margin: 0 10px;
+.card-text {
+  margin-bottom: 6px;
 }
-
-a {
-  color: #42b983;
+.card-body {
+  padding-bottom: 8px;
+}
+.card-subtitle,
+.card-link {
+  font-size: 12px;
 }
 </style>
